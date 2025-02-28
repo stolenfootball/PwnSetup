4bf08aadce5a9e5e909f2b42da5c3fbcb3f599b9
Jeremy Dunn <43766197+stolenfootball@users.noreply.github.com>
2025-02-28 15:07:36 -0500

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,43 @@
 mkdir $HOME/.pwnconfig
 
-
 # Download/setup pwndbg
+echo "##########################################"
+echo "#                                        #"
+echo "#           Installing PwnDbg            #"
+echo "#                                        #"
+echo "##########################################"
+echo ""
 git clone https://github.com/pwndbg/pwndbg.git $HOME/.pwnconfig/pwndbg
 chmod +x $HOME/.pwnconfig/pwndbg/setup.sh
-$HOME/.pwnconfig/pwndbg/setup.sh
+
+cd $HOME/.pwnconfig/pwndbg
+./setup.sh
 
 # Download splitmind
-git clone https://github.com/jerdna-regeiz/splitmind.git
+echo "##########################################"
+echo "#                                        #"
+echo "#         Installing Splitmind           #"
+echo "#                                        #"
+echo "##########################################"
+echo ""
+
+git clone https://github.com/jerdna-regeiz/splitmind.git $HOME/.pwnconfig/splitmind
 
 # Apply everything
+echo "##########################################"
+echo "#                                        #"
+echo "#            Applying Configs            #"
+echo "#                                        #"
+echo "##########################################"
+echo ""
+
 echo "source $HOME/.pwnconfig/pwndbg/gdbinit.py" >> $HOME/.gdbinit 
 echo "source $HOME/.pwnconfig/splitmind/gdbinit.py" >> $HOME/.gdbinit
 cat ./splitmind.conf >> $HOME/.gdbinit
+
+echo "##########################################"
+echo "#                                        #"
+echo "#               Finished!                #"
+echo "#                                        #"
+echo "##########################################"
+echo ""
